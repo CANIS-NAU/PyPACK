6398327fed347d0d46a768a6dce72020afa02e3e
Tyler Thatcher <tthatcher84@gmail.com>
2020-03-18 17:10:34 -0700

    Deleted pycaches

--- tests/osx/time_series/test_user.py
+++ tests/osx/time_series/test_user.py
@@ -1,9 +1,9 @@
 import pytest
 import libpypack.examples.location_files as loc_file
-import libpypack.time_series.user_info as ts
+from libpypack.time_series.user_info import get_user_df
 import pandas as pd
 
 def test_map_generator():
     tweet_df = pd.read_csv(loc_file.__path__[0] + '/sample_parsed_locs.csv')
-    user_df = ts.get_user_df(tweet_df, 12759492)
+    user_df = get_user_df(tweet_df, 12759492)
     assert len(user_df) == 3
